6b465fe76c514ad4923e70066c18de39a8c03323
Anniepoo <annie66us@yahoo.com>
2015-01-11 13:59:25 -0800

    Settings are now used to set the location of the contentteam repo. Gio, you'll need to read instructions in load.pl

--- load.pl
+++ load.pl
@@ -3,8 +3,24 @@
 
 */
 
+%
+%  For uses who clone the contentteam project into the same directory as
+%  the parent of this project this 'just works'. If your backend is
+%  elsewhere, you need to run once, which will break, then do this at
+%  top level ?- set_setting(backend_location, 'path/to/my/backend'). ?-
+%  save_settings().
+%
+%  DO NOT check in settings.db
+%
+:- setting(backend_location, atom, '../contentteam' , 'Location of backend code').
+
+:- load_settings('settings.db').
+
+user:file_search_path(backend, Location) :-
+	setting(load:backend_location, Location).
+
 % A bit of possibly excessive abstraction, we load the
-% server but don't run it in strangeloop.pl
+% server but don't run it in expertsystem.pl
 :-use_module(strangeloop).
 % make sure the handlers get loaded
 :- ensure_loaded(html_handlers).
